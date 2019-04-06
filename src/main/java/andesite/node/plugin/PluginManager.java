6d8b3d3fa0470d6124fc63a8a9dfcd1b4079c237
natanbc <natanbc@users.noreply.github.com>
2019-04-06 13:59:03 -0300

    properly load reference.conf from plugins

--- src/main/java/andesite/node/plugin/PluginManager.java
+++ src/main/java/andesite/node/plugin/PluginManager.java
@@ -10,16 +10,23 @@ import gg.amy.singyeong.Dispatch;
 import io.vertx.core.json.JsonObject;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class PluginManager {
+    private static final Logger log = LoggerFactory.getLogger(PluginManager.class);
+    
     private final List<PluginLoader> loaders = new ArrayList<>();
     private final List<Plugin> plugins = new ArrayList<>();
     private final List<String> loadedPlugins = new ArrayList<>();
@@ -37,7 +44,9 @@ public class PluginManager {
     
     @Nonnull
     public Class<?> findClass(@Nonnull String name) throws ClassNotFoundException {
+        log.info("Attempting to find plugin class {}", name);
         for(var loader : loaders) {
+            log.debug("Attempting to load class {} from loader {}", name, loader);
             try {
                 return loader.loadClass(name);
             } catch(ClassNotFoundException ignored) {
@@ -48,6 +57,7 @@ public class PluginManager {
     
     @Nonnull
     public <T> T loadHandler(@Nonnull Class<T> type, @Nonnull String name) {
+        log.info("Loading handler {} of type {}", name, type.getName());
         try {
             var c = findClass(name);
             if(!type.isAssignableFrom(c)) {
@@ -80,7 +90,16 @@ public class PluginManager {
     public Config applyPluginDefaults(@Nonnull Config config) {
         for(var l : loaders) {
             if(l.hasFile("reference.conf")) {
-                config = config.withFallback(ConfigFactory.parseResources(l, "reference.conf"));
+                log.info("Loading reference.conf from {}", l);
+                try(var reader = new BufferedReader(new InputStreamReader(
+                        Objects.requireNonNull(l.openFile("reference.conf")))
+                )) {
+                    var reference = ConfigFactory.parseReader(reader);
+                    log.debug("Loaded reference.conf from {}: {}", l, reference.root().render());
+                    config = config.withFallback(reference);
+                } catch(IOException e) {
+                    throw new IllegalStateException("reference.conf exists but cannot be read", e);
+                }
             }
         }
         return config;
@@ -88,18 +107,21 @@ public class PluginManager {
     
     public void init() {
         for(var p : plugins) {
+            log.debug("Initializing plugin {}", p);
             p.init(state);
         }
     }
     
     public void configurePlayerManager(@Nonnull AudioPlayerManager manager) {
         for(var p : plugins) {
+            log.debug("Configuring player manager {} with plugin {}", manager, p);
             p.configurePlayerManager(state, manager);
         }
     }
     
     public boolean requiresRouter() {
         for(var p : plugins) {
+            log.debug("Checkinf ig plugin {} requires router", p);
             if(p.requiresRouter()) {
                 return true;
             }
@@ -109,6 +131,7 @@ public class PluginManager {
     
     public void configureRouter(@Nonnull Router router) {
         for(var p : plugins) {
+            log.debug("Configuring router with plugin {}", p);
             p.configureRouter(state, router);
         }
     }
@@ -116,6 +139,7 @@ public class PluginManager {
     public boolean startListeners() {
         var started = false;
         for(var p : plugins) {
+            log.debug("Starting listeners from plugin {}", p);
             started |= p.startListeners(state);
         }
         return started;
@@ -123,7 +147,9 @@ public class PluginManager {
     
     public boolean customHandleHttpRequest(@Nonnull RoutingContext context) {
         for(var p : plugins) {
+            log.debug("Calling custom http handling of plugin {}", p);
             if(p.onRawHttpRequest(state, context) == Plugin.HookResult.ABORT) {
+                log.debug("Request handled");
                 return true;
             }
         }
@@ -132,7 +158,9 @@ public class PluginManager {
     
     public boolean customHandleWebSocketPayload(@Nonnull WebSocketState state, @Nonnull JsonObject payload) {
         for(var p : plugins) {
+            log.debug("Calling custom ws handling of plugin {}", p);
             if(p.onRawWebSocketPayload(this.state, state, payload) == Plugin.HookResult.ABORT) {
+                log.debug("Payload handled");
                 return true;
             }
         }
@@ -141,7 +169,9 @@ public class PluginManager {
     
     public boolean customHandleSingyeongPayload(@Nonnull Dispatch dispatch) {
         for(var p : plugins) {
+            log.debug("Calling custom singyeong handling of plugin {}", p);
             if(p.onRawSingyeongPayload(state, dispatch) == Plugin.HookResult.ABORT) {
+                log.debug("Payload handled");
                 return true;
             }
         }
