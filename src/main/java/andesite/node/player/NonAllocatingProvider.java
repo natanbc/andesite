f032b26ea4ee0641ffb5844f5e39655622979849
natanbc <natanbc@users.noreply.github.com>
2019-07-31 00:39:49 -0300

    temporarily fix audio until i figure out why newer magma versions don't work

--- src/main/java/andesite/node/player/NonAllocatingProvider.java
+++ src/main/java/andesite/node/player/NonAllocatingProvider.java
@@ -7,32 +7,12 @@ import com.sedmelluq.discord.lavaplayer.track.playback.MutableAudioFrame;
 
 import javax.annotation.CheckReturnValue;
 import javax.annotation.Nonnull;
-import java.lang.reflect.Field;
 import java.nio.ByteBuffer;
-import java.util.function.Function;
 
 class NonAllocatingProvider implements AudioProvider {
-    private static final Function<MutableAudioFrame, ByteBuffer> INTERNAL_BUFFER_GETTER;
-    
     private final ByteBuffer buffer = ByteBuffer.allocate(StandardAudioDataFormats.DISCORD_OPUS.maximumChunkSize());
     private final MutableAudioFrame frame = new MutableAudioFrame();
     private final AudioPlayer player;
-    
-    static {
-        try {
-            Field f = MutableAudioFrame.class.getDeclaredField("frameBuffer");
-            f.setAccessible(true);
-            INTERNAL_BUFFER_GETTER = frame -> {
-                try {
-                    return (ByteBuffer) f.get(frame);
-                } catch(Exception impossible) {
-                    throw new AssertionError(impossible);
-                }
-            };
-        } catch(Exception e) {
-            throw new ExceptionInInitializerError(e);
-        }
-    }
     
     public NonAllocatingProvider(AudioPlayer player) {
         this.player = player;
@@ -50,14 +30,6 @@ class NonAllocatingProvider implements AudioProvider {
     @Nonnull
     @Override
     public ByteBuffer provide() {
-        if(INTERNAL_BUFFER_GETTER.apply(frame) != buffer) {
-            frame.getData(buffer.array(), frame.getDataLength());
-            //hopefully this copy won't be needed for the next frame
-            //and data will be directly written to the provider's buffer.
-            //this call is pretty much free, and might help preventing
-            //more copies for future frames.
-            frame.setBuffer(buffer.limit(buffer.capacity()));
-        }
         return buffer.position(0).limit(frame.getDataLength());
     }
     
