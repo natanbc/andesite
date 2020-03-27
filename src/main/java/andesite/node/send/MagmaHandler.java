72babe2febc3f00c3128b66c2ee6a0ecafbea850
Napster <napster@npstr.space>
2020-03-27 21:01:39 +0100

    Pin TLSv1.2

--- src/main/java/andesite/node/send/MagmaHandler.java
+++ src/main/java/andesite/node/send/MagmaHandler.java
@@ -9,10 +9,13 @@ import net.dv8tion.jda.api.audio.AudioSendHandler;
 import net.dv8tion.jda.api.audio.factory.IAudioSendFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import space.npstr.magma.MagmaApi;
-import space.npstr.magma.MagmaMember;
-import space.npstr.magma.MagmaServerUpdate;
-import space.npstr.magma.events.api.WebSocketClosed;
+import org.xnio.OptionMap;
+import org.xnio.Options;
+import space.npstr.magma.MagmaFactory;
+import space.npstr.magma.api.MagmaApi;
+import space.npstr.magma.api.MagmaMember;
+import space.npstr.magma.api.MagmaServerUpdate;
+import space.npstr.magma.api.event.WebSocketClosed;
 
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -28,7 +31,7 @@ public class MagmaHandler implements AudioHandler {
     
     public MagmaHandler(Andesite andesite) {
         var factory = createSendFactory(andesite);
-        this.magma = MagmaApi.of(__ -> factory);
+        this.magma = MagmaFactory.of(__ -> factory, OptionMap.EMPTY, OptionMap.create(Options.SSL_PROTOCOL, "TLSv1.2"));
         magma.getEventStream().subscribe(event -> {
             if(event instanceof WebSocketClosed) {
                 var e = (WebSocketClosed) event;
