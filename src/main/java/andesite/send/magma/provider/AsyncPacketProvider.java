33a05cd29643e41286cba2248cafbfbe7966e72b
natanbc <natanbc@users.noreply.github.com>
2020-10-03 02:13:56 -0300

    move AsyncPacketProvider to magma dir

--- src/main/java/andesite/send/magma/provider/AsyncPacketProvider.java
+++ src/main/java/andesite/send/magma/provider/AsyncPacketProvider.java
@@ -1,4 +1,4 @@
-package andesite.provider;
+package andesite.send.magma.provider;
 
 import net.dv8tion.jda.api.audio.factory.IPacketProvider;
 import net.dv8tion.jda.api.audio.hooks.ConnectionStatus;
