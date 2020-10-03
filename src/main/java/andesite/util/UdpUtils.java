a21119fdd6c2416f1627863c5e05699584f6b381
natanbc <natanbc@users.noreply.github.com>
2020-10-03 18:47:57 -0300

    include netty natives, fix broken udp queue detection

--- src/main/java/andesite/util/UdpUtils.java
+++ src/main/java/andesite/util/UdpUtils.java
@@ -1,6 +1,5 @@
 package andesite.util;
 
-import com.sedmelluq.discord.lavaplayer.natives.NativeLibLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,8 +13,16 @@ public class UdpUtils {
         if(available != null) {
             return available;
         }
+        //some dependency has LP's NativeLibLoader available on the compile classpath
+        //but gradle's dependency resolution ends up removing that, so this would silently
+        //fail because it couldn't find NativeLibLoader, so just call directly the
+        //queue manager library to trigger the load. no native resources are allocated
+        //by the getInstance call itself, so this won't leak resources.
         try {
-            NativeLibLoader.load(Class.forName(QUEUE_MANAGER_LIBRARY), "udpqueue");
+            var klass = Class.forName(QUEUE_MANAGER_LIBRARY);
+            var method = klass.getDeclaredMethod("getInstance");
+            method.setAccessible(true);
+            method.invoke(null);
             log.info("Loaded udp-queue library");
             return available = true;
         } catch(Throwable t) {
